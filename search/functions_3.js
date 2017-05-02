9a96c9a43d93381f503ad43875697da3134426f9
Peter Farley <far.peter1@gmail.com>
2017-05-01 22:16:59 -0500

    Updated documentation

--- search/functions_3.js
+++ search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['decode',['decode',['../classHuffmanDecoder.html#afa062b432ceb4a4df977e80e96c08bd8',1,'HuffmanDecoder']]]
+  ['decode',['decode',['../classHuffmanDecoder.html#afa062b432ceb4a4df977e80e96c08bd8',1,'HuffmanDecoder.decode(String code)'],['../classHuffmanDecoder.html#a6e951a5ea204b7c96ca1f676c5d77121',1,'HuffmanDecoder.decode(BitInputStream input)']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['encode',['encode',['../classHuffmanEncoder.html#a5fbfc55263a3c4832bac430c9cd4ecc7',1,'HuffmanEncoder']]]
+  ['encode',['encode',['../classHuffmanEncoder.html#a5fbfc55263a3c4832bac430c9cd4ecc7',1,'HuffmanEncoder.encode(T[] input)'],['../classHuffmanEncoder.html#accb42cd3e68921d98e54e9cb3275550c',1,'HuffmanEncoder.encode(T[] input, BitOutputStream output)']]]
 ];
